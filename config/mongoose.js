3c737a870bf302ac2db07e049e30a0d12143ab11
WANG Yi <31856747+victorien1007@users.noreply.github.com>
2018-03-21 11:14:11 +0100

    cnm

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ var mongoose=require('mongoose');
 var config=require('./db_url.js');
 
 module.exports=function(){
-    var db=mongoose.connect(config.mongodb);
+      var db=mongoose.connect(config.mongodb, { keepAlive: 30000, connectTimeoutMS: 30000, }, function(err, db) {});
     require('../model/user.js');
     require('../model/holiday.js');
     require('../model/signin.js');
