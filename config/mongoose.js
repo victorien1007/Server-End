eceb7b58312f5e4d06f0a1ce339754ab58494ab5
WANG Yi <31856747+victorien1007@users.noreply.github.com>
2018-03-21 11:27:59 +0100

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,9 +3,21 @@
  */
 var mongoose=require('mongoose');
 var config=require('./db_url.js');
-
+var options = { 
+  server: { 
+    socketOptions: { 
+      keepAlive: 300000, connectTimeoutMS: 30000 
+    } 
+  }, 
+  replset: { 
+    socketOptions: { 
+      keepAlive: 300000, 
+      connectTimeoutMS : 30000 
+    } 
+  } 
+};
 module.exports=function(){
-      var db=mongoose.connect(config.mongodb, { keepAlive: 30000, connectTimeoutMS: 30000, }, function(err, db) {});
+      var db=mongoose.connect(config.mongodb,options);
     require('../model/user.js');
     require('../model/holiday.js');
     require('../model/signin.js');
