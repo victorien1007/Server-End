18e3e7e8d81be14593208fff8b2cf4dbfdba2db0
WANG Yi <31856747+victorien1007@users.noreply.github.com>
2018-03-21 14:50:39 +0100

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -17,7 +17,12 @@ var options = {
   } 
 };
 module.exports=function(){
-      var db=mongoose.connect(config.mongodb);
+      var db=mongoose.connect(config.mongodb,{
+      poolSize: 10,
+      promiseLibrary: global.Promise,
+      keepAlive: 300000, 
+      connectTimeoutMS : 30000 
+    });
 
     require('../model/user.js');
     require('../model/holiday.js');
