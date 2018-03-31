c381fcf3ba2097d7c95be29805a029ef2a9cd2be
WANG Yi <31856747+victorien1007@users.noreply.github.com>
2018-03-31 19:38:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,21 @@ var Signin = require('../controller/signin');
 
 
 module.exports = function(app) {
-  /*处理用户登录请求*/
+  /*Handle user login requests*/
   app.post('/phone_api/login',User.login);
-  /*处理用户登录请求*/
+  /*Handle user login requests*/
   app.post('/phone_api/register',User.register);
 
-  /*用户提交请假请求*/
+  /*User submits leave request*/
   app.post('/phone_api/add_holiday',Holiday.addHoliday);
-  /*获取用户提交请假记录*/
+  /*Get users to submit leave records*/
   app.post('/phone_api/get_holidays',Holiday.getHolidays);
-  /*用户签到*/
+  /*User sign in*/
   app.post('/phone_api/sign_in',Signin.signin);
-  /*用户签到签到记录*/
+  /*User check-in record*/
   app.post('/phone_api/get_signins',Signin.getSignins);
+
+  app.get('/',function(req,res){
+    res.render('home') } );
 
 };
